f4de49952a66aa5f7e3d192ab2a1c6574f3e1260
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-08 22:54:03 +0800

    n

--- InsertSort.cpp
+++ InsertSort.cpp
@@ -10,6 +10,51 @@ InsertSort<Type>::InsertSort(int len)
   data=new Type[length];
 }
 
+template <class Type> void InsertSort<Type>::Print()
+{
+  for(int i=0;i<length;i++)
+  {
+    cout<<data[i]<<",";
+  }
+  cout<<endl;
+}
+
+template <class Type> void InsertSort<Type>::Sort()
+{
+  int k,j,left,right;
+  Type temp;
+  for(int i=0;i<length-1;i++)	
+  {
+    j=i+1;
+    if(data[i]>data[j])//if the data[j] is smaller, then, find appropriate place in the previous i datas for it.
+    {
+      left=0;
+      right=i;
+      temp=data[j];
+      while(left<right-1)
+      {
+        if(temp<data[(right+left)/2])
+        {
+          right=(right+left)/2;
+        }
+        else
+        {
+          left=(right+left)/2;
+        }
+      }
+      for(;j>left;j--)
+        data[j]=data[j-1];
+      if(temp>data[left])
+      {
+        data[right]=temp;
+      }
+      else
+      {
+        data[left]=temp;
+      }
+    }
+  }
+}
 int main() 
 {
   InsertSort<int>* insertSort=new InsertSort<int>(10);
